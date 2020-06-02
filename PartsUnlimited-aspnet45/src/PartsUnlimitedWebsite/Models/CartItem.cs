b2828e62c39dd64042f205996cc448f8f466e629
sreediva <sreeediva@hotmail.com>
2020-06-01 23:12:07 -0700

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My first comment
+        //My second comment
         [Key]
         public int CartItemId { get; set; }
 
